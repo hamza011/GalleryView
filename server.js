da71e1f550493c50c98ead024d80dfbf61d0829c
Hamza Ahmed <h.hamza011@gmail.com>
2018-05-14 14:09:06 +0500

    practice

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('index');
+    // res.render('index');
+    res.sendFile('index.html');
 });
 
 app.listen(port, function() {
